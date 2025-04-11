7cec00a1f93937b97cc9dccce75586724105e108
Duc Hai Doan <111358742+haibeu2901@users.noreply.github.com>
2025-04-11 12:40:33 +0700

    Add files via upload

---
+++ lmsDb.sql
@@ -0,0 +1,12 @@
+create database LibraryManagementSystem;
+
+use LibraryManagementSystem;
+
+create table tbl_Student ();
+
+create table tbl_Book ();
+
+create table tbl_Issue ();
+
+create table tbl_User ();
+
